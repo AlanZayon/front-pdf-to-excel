6fe0ddb68311a99b76fcef2f1407da0d1b3d92e6
Alan Zayon <alanzayon72@gmail.com>
2025-05-03 12:14:59 -0300

    Update FileDownloadService.ts

--- src/infrastructure/services/FileDownloadService.ts
+++ src/infrastructure/services/FileDownloadService.ts
@@ -12,7 +12,7 @@ export class FileDownloadService {
       const url = window.URL.createObjectURL(new Blob([response.data]))
       const link = document.createElement('a')
       link.href = url
-      link.setAttribute('download', 'relatorio.xlsx') 
+      link.setAttribute('download', 'relatorio.csv') 
       document.body.appendChild(link)
       link.click()
       link.remove()
