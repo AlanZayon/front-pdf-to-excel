30be53cbfe95fd8b601f922e49b086fbd8db36ef
Alan Zayon <alanzayon72@gmail.com>
2025-05-21 11:07:20 -0300

    Update FileDownloadService.ts

--- src/infrastructure/services/FileDownloadService.ts
+++ src/infrastructure/services/FileDownloadService.ts
@@ -12,7 +12,7 @@ export class FileDownloadService {
       const url = window.URL.createObjectURL(new Blob([response.data]))
       const link = document.createElement('a')
       link.href = url
-      link.setAttribute('download', 'relatorio.csv') 
+      link.setAttribute('download', 'PGTO.csv') 
       document.body.appendChild(link)
       link.click()
       link.remove()
