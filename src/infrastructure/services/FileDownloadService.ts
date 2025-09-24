f6cec0b054d7e122fc81f77385c0b09fdceebe71
AlanZayon <alanzayon72@gmail.com>
2025-09-24 11:35:29 -0300

    Downlod file name to document type

--- src/infrastructure/services/FileDownloadService.ts
+++ src/infrastructure/services/FileDownloadService.ts
@@ -8,15 +8,38 @@ export class FileDownloadService {
         responseType: 'blob',
         withCredentials: true
       })
-      console.log(response.data)
+
+      const contentDisposition = response.headers['content-disposition']
+      let fileName = 'PGTO.csv'
+
+      console.log('Content-Disposition header:', contentDisposition) 
+
+      if (contentDisposition) {
+
+        const fileNameMatch = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/i)
+        
+        if (fileNameMatch && fileNameMatch[1]) {
+
+          fileName = fileNameMatch[1].replace(/['"]/g, '')
+          
+          if (fileName.includes("UTF-8''")) {
+            fileName = fileName.split("UTF-8''")[1]
+          }
+        }
+      }
+
+      console.log('Filename extracted:', fileName)
 
       const url = window.URL.createObjectURL(new Blob([response.data]))
       const link = document.createElement('a')
       link.href = url
-      link.setAttribute('download', 'PGTO.csv') 
+      link.setAttribute('download', fileName)
       document.body.appendChild(link)
       link.click()
       link.remove()
+      window.URL.revokeObjectURL(url)
+
+      logger.info(`Download realizado com sucesso: ${fileName}`)
 
     } catch (error) {
       logger.error('Erro ao realizar download', error)
